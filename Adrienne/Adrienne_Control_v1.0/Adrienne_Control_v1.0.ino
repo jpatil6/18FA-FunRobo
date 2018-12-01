457ea6bcd18e719e1f7ae4ee05aa1ab5ba155ba7
Viktor Deturck <vdeturck@olin.edu>
2018-12-01 16:32:22 -0500

    Cleaning up + IRs

--- Adrienne/Adrienne_Control_v1.0/Adrienne_Control_v1.0.ino
+++ Adrienne/Adrienne_Control_v1.0/Adrienne_Control_v1.0.ino
@@ -1,31 +1,32 @@
 /*****************************************************************************************
- * Title: 2018 (ENGR3390 final project 1)
- * Description: This structure template contains a SENSE-THINK-ACT flow to
- * allow a Robot to perform a sequence of meta-behaviors in soft-real-time based on direct 
- * text commands from a human operator.
- * Robot Name: Simple PixyCam sensor
- * What does code do: poll open, sense: object recognition data from PixyCam camera every second, 
- * think: whether the detected object and its location is a target or hole, 
- * act: print the target/hole status and location of detected objects,
- * carry out operator text input, loop indefinitely
- * Hardware warnings: Do not wire the six headed arduino I/O pin the wrong way around.
- * Created by J.Patil October 2018
+   Title: 2018 (ENGR3390 final project 1)
+   Description: This structure template contains a SENSE-THINK-ACT flow to
+   allow a Robot to perform a sequence of meta-behaviors in soft-real-time based on direct
+   text commands from a human operator.
+   Robot Name: Simple PixyCam sensor
+   What does code do: poll open, sense: object recognition data from PixyCam camera every second,
+   think: whether the detected object and its location is a target or hole,
+   act: print the target/hole status and location of detected objects,
+   carry out operator text input, loop indefinitely
+   Hardware warnings: Do not wire the six headed arduino I/O pin the wrong way around.
+   Created by J.Patil October 2018
  * ***************************************************************************************
- */
+*/
 
-// 
-// TODO: 
+//
+// TODO:
 //      SENSE:
-//      THINK: 
-//      ACT: 
+//      THINK:
+//      ACT:
 
 //========================================================================================
 // Load supporting Arduino Libraries
 //========================================================================================
-#include <Servo.h>    //example of loading ServoMotors library
-#include <Pixy.h>
-#include <SPI.h>
-#include <SharpIR.h>
+#include <Servo.h>        //ServoMotors library
+#include <Pixy.h>         // Pixy Library
+#include <SPI.h>          // 
+#include <SharpIR.h>      // IR sensors library
+#include <TugNeoPixel.h>  //NeoPixel Ring library
 
 //========================================================================================
 // Create and initialize global variables, objects and constants (containers for all data)
@@ -38,6 +39,8 @@ unsigned long newLoopTime = 0;    //create a name for new loop time in milliseco
 unsigned long cycleTime = 0;      //create a name for elapsed loop cycle time
 const long controlLoopInterval = 1000; //create a name for control loop cycle time in milliseconds
 
+TugNeoPixel neo = TugNeoPixel(7, 16);  //initialize NeoPixel object
+
 //Pixy variables
 
 //Sonar variables
@@ -46,9 +49,16 @@ const int sonar1 = 0;  //sets signal pin for first sonar sensor
 const int sonar2 = 1;  //sets signal pin for second sonar sensor
 const int sonar3 = 2;  //sets signal pin for third sonar sensor
 int trigger = 13;  //sets 1 trigger pin for all 3 sensors
-float sonarArray = {0,0,0};
+float sonarArray[3] = {0, 0, 0};
 
 //IR variables
+SharpIR IR1(SharpIR::GP2Y0A02YK0F, A0);
+SharpIR IR2(SharpIR::GP2Y0A02YK0F, A1);
+SharpIR IR3(SharpIR::GP2Y0A02YK0F, A2);
+SharpIR IR4(SharpIR::GP2Y0A02YK0F, A3);
+SharpIR IR5(SharpIR::GP2Y0A02YK0F, A4);
+SharpIR IR6(SharpIR::GP2Y0A02YK0F, A5);
+int IRarray[6] = {0, 0, 0, 0, 0, 0};
 
 //Think variables
 
@@ -65,11 +75,12 @@ Servo propellor;
 //=========================================================================================
 void setup() {      // Step 1)Put your robot setup code here, to run once:
   Serial.begin(9600);                 // start serial communications
+  neo.begin();                        // start the NeoPixel
   Serial.println(" Robot Controller Starting Up! Watch your fingers! ");
 
   // Step 2)Put your robot mission setup code here, to run once:
   // Add mission code here
-  
+
   //Pixy initializing
 
   //Sonar initializing
@@ -105,7 +116,7 @@ void loop() {
 
   // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
   // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
-  while(realTimeRunStop == true) {      // if OCU-Stop not commanded, run control loop
+  while (realTimeRunStop == true) {     // if OCU-Stop not commanded, run control loop
     // Check if operator inputs a command during real-time loop eecution
     if (Serial.available() > 0) {     // check to see if operator typed at OCU
       realTimeRunStop = false;      // if OCU input typed, stop control loop
@@ -113,33 +124,35 @@ void loop() {
       //Serial.println("command 2 is "+command);
       break;      // break out of real-time loop
     }
-    else {realTimeRunStop = true;}    // if no operator input, run real-time loop
+    else {
+      realTimeRunStop = true; // if no operator input, run real-time loop
+    }
 
     // Real-Time clock control. Check to see if one clock cycle has elapesed before running this control code
     newLoopTime = millis();           // get current Arduino time (50 days till wrap)
     if (newLoopTime - oldLoopTime >= controlLoopInterval) { // if true run flight code
       oldLoopTime = newLoopTime;      // reset time stamp
-      blinkAliveLED();                // toggle blinky alive light
 
 
-  //SENSE-sense---sense---sense---sense---sense---sense---sense---sense---sense---sense---sense-------
 
-    // TODO add sensor code here
+      //SENSE-sense---sense---sense---sense---sense---sense---sense---sense---sense---sense---sense-------
 
-  // THINK think---think---think---think---think---think---think---think---think---think---think---------
-    // pick robot behavior based on operator input command typed at console
+      // TODO add sensor code here
 
-      if ( command == "stop"){
+      // THINK think---think---think---think---think---think---think---think---think---think---think---------
+      // pick robot behavior based on operator input command typed at console
+
+      if ( command == "stop") {
         Serial.println("Stop Robot");
         realTimeRunStop = false;    //exit real time control loop
         break;
       }
-      else if (command == "move"){  //Move robot to Operator commanded position
+      else if (command == "move") { //Move robot to Operator commanded position
         Serial.println("Move robot ");
         Serial.println("Type stop to stop robot");
         realTimeRunStop = true;     //don't exit loop after running once
       }
-      else if (command == "idle"){  //Make robot alive with small motions
+      else if (command == "idle") { //Make robot alive with small motions
         Serial.println("Idle Robot");
         Serial.println("Type stop to stop robot");
         realTimeRunStop = true;     //run loop continually
@@ -150,29 +163,27 @@ void loop() {
         realTimeRunStop = false;
       }
 
-  // ACT-act---act---act---act---act---act---act---act---act---act---act---act---act---act------------
+      // ACT-act---act---act---act---act---act---act---act---act---act---act---act---act---act------------
 
-      ESTOP = digitalRead(eStopPin);    // check ESTOP switch
 
-  // Check to see if all code ran successfully in one real-time increment
-      cycleTime = millis()-newLoopTime;     // calculate loop execution time
-      if( cycleTime > controlLoopInterval){
+      // Check to see if all code ran successfully in one real-time increment
+      cycleTime = millis() - newLoopTime;   // calculate loop execution time
+      if ( cycleTime > controlLoopInterval) {
         Serial.println("******************************************");
         Serial.println("error - real time has failed, stop robot!"); // loop took too long to run
         Serial.print(" 1000 ms real-time loop took = ");
         Serial.println(cycleTime);                                   // print loop time
         Serial.println("******************************************");
         break;          // break out of real-time inner loop
-        }
-        } // end of "if (newLoopTime - oldLoopTime >= controlLoopInterval)" real-time loop structure
-      } // end of "inner" "while(realTimeRunStop == true)" real-time control loop
-      // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
-      // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
+      }
+    } // end of "if (newLoopTime - oldLoopTime >= controlLoopInterval)" real-time loop structure
+  } // end of "inner" "while(realTimeRunStop == true)" real-time control loop
+  // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
+  // real-time-loop******real-time-loop******real-time-loop******real-time-loop******real-time-loop******
 
-    // SEND Robot State to Operator Control Unit-(OCU) ocu--- ocu-- ocu--- ocu--- ocu--- ocu-- -ocu--
-      Serial.println("================================================================");
-      Serial.println("| Robot control loop stopping to wait for new command ");   // send robot status
-      if (ESTOP == true) Serial.println("| Robot motors E-Stopped by external switch");   // send E-Stop
+  // SEND Robot State to Operator Control Unit-(OCU) ocu--- ocu-- ocu--- ocu--- ocu--- ocu-- -ocu--
+  Serial.println("================================================================");
+  Serial.println("| Robot control loop stopping to wait for new command ");   // send robot status
 
 } // end of "outer" void loop()
 
@@ -198,33 +209,42 @@ void loop() {
 
 // OCU functions ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu---ocu------------
 
-String getOperatorInput(){
-// This function prints operator command options on the serial console and prompts
-// operator to input desired robot command
-// Serial.println(" ");
-Serial.println("======================================================================================");
-Serial.println("| Robot Behavior-Commands: move(moves robot), stop(e-stops motors), idle(robot idles)|");
-Serial.println("|                                                                                    |");
-//Serial.println("====================================================================================");
-Serial.println("| Please type desired robot behavior in command line at top of this window           |");
-Serial.println("| and then press SEND button.                                                        |");
-Serial.println("======================================================================================");
-while (Serial.available()==0) {};   // do nothing until operator input typed
-command = Serial.readString();      // read command string
-//command.trim();
-Serial.print("| New robot behavior command is: ");    // give command feedback to operator
-Serial.println(command);
-Serial.println("| Type 'stop' to stop control loop and wait for new command                          |");
-Serial.println("======================================================================================");
-return command;
+String getOperatorInput() {
+  // This function prints operator command options on the serial console and prompts
+  // operator to input desired robot command
+  // Serial.println(" ");
+  Serial.println("======================================================================================");
+  Serial.println("| Robot Behavior-Commands: move(moves robot), stop(e-stops motors), idle(robot idles)|");
+  Serial.println("|                                                                                    |");
+  //Serial.println("====================================================================================");
+  Serial.println("| Please type desired robot behavior in command line at top of this window           |");
+  Serial.println("| and then press SEND button.                                                        |");
+  Serial.println("======================================================================================");
+  while (Serial.available() == 0) {}; // do nothing until operator input typed
+  command = Serial.readString();      // read command string
+  //command.trim();
+  Serial.print("| New robot behavior command is: ");    // give command feedback to operator
+  Serial.println(command);
+  Serial.println("| Type 'stop' to stop control loop and wait for new command                          |");
+  Serial.println("======================================================================================");
+  return command;
 }
 
 // SENSE functions sense---sense---sense---sense---sense---sense---sense---sense---sense---
 
+
 // Pixy function
 
 // IR function
-
+void readIR() {
+  /* Uses the getDistance function from SharpIR library to get distances in cm*/
+  IRarray[0] = IR1.getDistance();
+  IRarray[1] = IR2.getDistance();
+  IRarray[2] = IR3.getDistance();
+  IRarray[3] = IR4.getDistance();
+  IRarray[4] = IR5.getDistance();
+  IRarray[5] = IR6.getDistance();
+}
 // Sonar function
 
 float mapSonar(float reading)
@@ -232,18 +252,18 @@ float mapSonar(float reading)
   return 0.0 + (reading - 0.0) * (18.0 - 0.0) / (19.0 - 0.0);
 }
 
-void readSonar(){
+void readSonar() {
   /*
-  Scale factor is (Vcc/512) per inch. A 5V supply yields ~9.8mV/in
-  Arduino analog pin goes from 0 to 1024, so the value has to be divided by 2 to get the actual inches
+    Scale factor is (Vcc/512) per inch. A 5V supply yields ~9.8mV/in
+    Arduino analog pin goes from 0 to 1024, so the value has to be divided by 2 to get the actual inches
   */
-  digitalWrite(trigger,HIGH);
+  digitalWrite(trigger, HIGH);
   delay(1); //triggers the sonars/makes them take a reading
-  digitalWrite(trigger,LOW);
-  
-  sonarArray[0] = mapSonar(analogRead(sonar1)/2.0);
-  sonarArray[1] = mapSonar(analogRead(sonar2)/2.0);
-  sonarArray[2] = mapSonar(analogRead(sonar3)/2.0);
+  digitalWrite(trigger, LOW);
+
+  sonarArray[0] = mapSonar(analogRead(sonar1) / 2.0);
+  sonarArray[1] = mapSonar(analogRead(sonar2) / 2.0);
+  sonarArray[2] = mapSonar(analogRead(sonar3) / 2.0);
 }
 
 
