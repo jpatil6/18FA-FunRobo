109c084d395e2d80569e69a7abfee6afaf19902b
Viktor Deturck <vdeturck@olin.edu>
2018-12-08 19:59:06 -0500

    hmm

--- Adrienne/Adrienne_Control_v1.1/Adrienne_Control_v1.1.ino
+++ Adrienne/Adrienne_Control_v1.1/Adrienne_Control_v1.1.ino
@@ -451,28 +451,16 @@ void setHeading(int heading)
 }
 
 void swerveAroundIceberg(int side){
-  setHeading(1);
+  //setHeading(1);
+  if(sonarArray[1] > 100){
+    setHeading(9);
+  }
+  else{
+    setHeading(4);
+  }
+  
 }
 
-//maintainDistance
-void maintainDistance(int dist, int side)
-{
-  if(side==0){
-    if(objectArray[0]<0.9*dist)
-    {
-      setHeading(11);
-    }else if(objectArray[0]>1.1*dist){
-      setHeading(7);
-    }
-  }else if(side==1){
-    if(objectArray[18]<0.9*dist)
-    {
-      setHeading(11);
-    }else if(objectArray[18]>1.1*dist){
-      setHeading(7);
-    }    
-  }
-}
 // Voting Function
 // Takes the gaussian functions from find object and find target and outputs an angle
 // and a distance to the point we want to go to.
