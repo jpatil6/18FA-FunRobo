20cc2921be321d93db3cb658663df1fc15eed162
Eamon <eamon@piratecat.com>
2018-12-12 21:17:33 -0500

    aaaa

--- Adrienne/libraries/AdrienneBehaviors/AdrienneBehaviors.h
+++ Adrienne/libraries/AdrienneBehaviors/AdrienneBehaviors.h
@@ -1,1 +1,22 @@
-// AdrienneBehaviors.h
+/*
+  AdrienneBehaviors.h - Library for describing all the mission behaviors
+  Adrienne has
+  Created by Eamon, October 27, 2018.
+*/
+#ifndef AdrienneBehaviors_h
+#define AdrienneBehaviors_h
+
+#include "Arduino.h"
+
+class AdrienneBehaviors
+{
+public:
+  AdrienneBehaviors();
+  void fig8();
+  void wallfollow();
+  void fig8Dock();
+  void hunt();
+private:
+};
+
+#endif
